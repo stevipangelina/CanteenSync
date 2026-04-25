d802af56f9d72575c0f9d43322b2293588c022b5
Tazaina <tahtark39@gmail.com>
2026-04-25 15:07:03 +0700

    perbaikan router register

--- web-Canteen/routes/web.php
+++ web-Canteen/routes/web.php
@@ -1,23 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+//use App\Http\Controllers\KantinController;
 
-Route::get('/login', [AuthController::class, 'showLogin']);
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/register', [AuthController::class, 'showRegister']);
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-//Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware('auth');
+//Route::get('/dashboard', [KantinController::class, 'dashboard']);  #->middleware('auth');
 
-//Route::get('/logout', [AuthController::class, 'logout']);
-
-// Route::get('/register', [AuthController::class,'showRegister']);
-// Route::post('/register', [AuthController::class,'register']);
-
-// Route::get('/login', [AuthController::class,'showLogin']);
-// Route::post('/login', [AuthController::class,'login']);
-
-// Route::get('/dashboard', [AuthController::class,'dashboard'])->middleware('auth');
-
-// Route::get('/logout', [AuthController::class,'logout']);
