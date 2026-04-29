9c907013128f77d72405f87492f80d7537bc3bac
Tazaina <tahtark39@gmail.com>
2026-04-29 21:28:53 +0700

    update menu kantin controller

--- web-Canteen/app/Http/Controllers/KantinController.php
+++ web-Canteen/app/Http/Controllers/KantinController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 
 class KantinController extends Controller
@@ -8,9 +9,21 @@ class KantinController extends Controller
     public function dashboard()
     {
         $canteens = [
-            ['name' => 'Kantin A', 'image' => 'canteen1.png'],
-            ['name' => 'Kantin B', 'image' => 'canteen2.png'],
-            ['name' => 'Kantin C', 'image' => 'canteen3.png'],
+            [
+                'id' => 1,
+                'name' => 'Kantin A',
+                'image' => 'canteen1.png'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Kantin B',
+                'image' => 'canteen2.png'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Kantin C',
+                'image' => 'canteen3.png'
+            ],
         ];
 
         return view('dashboard', compact('canteens'));
