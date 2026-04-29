d2ad83fff008b772646388e18f83198975ca37f8
Tazaina <tahtark39@gmail.com>
2026-04-29 21:26:40 +0700

    tambah router checkout dari keranjang dan pesanan

--- web-Canteen/routes/web.php
+++ web-Canteen/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KantinController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\KeranjangController;
-//use App\Http\Controllers\PemesananController;
+use App\Http\Controllers\PemesananController;
 
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
@@ -23,7 +23,7 @@ Route::get('/keranjang', [KeranjangController::class, 'index'])->name('keranjang
 Route::post('/keranjang/update', [KeranjangController::class, 'update'])->name('keranjang.update');
 Route::get('/keranjang/hapus/{id}', [KeranjangController::class, 'hapus'])->name('keranjang.hapus');
 
-// Route::get('/checkout', [PemesananController::class, 'index'])->name('checkout');
-// Route::post('/checkout/simpan', [PemesananController::class, 'simpan'])->name('checkout.simpan');
+Route::get('/checkout', [PemesananController::class, 'index'])->name('checkout');
+Route::post('/checkout/simpan', [PemesananController::class, 'simpan'])->name('checkout.simpan');
 
-// Route::get('/pesanan/sukses', [PemesananController::class, 'sukses']);
+Route::get('/pesanan/sukses', [PemesananController::class, 'sukses']);
