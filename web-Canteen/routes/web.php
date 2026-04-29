9985bb3d2c7b81a983ee2a594c1758de5201c132
Tazaina <tahtark39@gmail.com>
2026-04-29 19:21:24 +0700

    menambahkan router  dashboard

--- web-Canteen/routes/web.php
+++ web-Canteen/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-//use App\Http\Controllers\KantinController;
+use App\Http\Controllers\KantinController;
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
@@ -9,5 +9,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-//Route::get('/dashboard', [KantinController::class, 'dashboard']);  #->middleware('auth');
+Route::get('/dashboard', [KantinController::class, 'dashboard']);  #->middleware('auth');
 
