d8cb4b7b9192c973fa4d10cfcc2cb94137b43a05
Tazaina <tahtark39@gmail.com>
2026-04-29 20:24:04 +0700

    tambah menu kantin

--- web-Canteen/routes/web.php
+++ web-Canteen/routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KantinController;
+use App\Http\Controllers\MenuController;
+
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
@@ -10,4 +12,7 @@ Route::get('/register', [AuthController::class, 'showRegister'])->name('register
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/dashboard', [KantinController::class, 'dashboard']);  #->middleware('auth');
+
+Route::get('/', [KantinController::class, 'index']);
+Route::get('/kantin/{id}', [MenuController::class, 'index']);
 
