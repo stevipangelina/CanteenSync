39e997705bc8944faf1254d02dc8da93b33e7d8c
Tazaina <tahtark39@gmail.com>
2026-05-30 23:51:38 +0700

    update model akun

--- web-Canteen/app/Models/Akun.php
+++ web-Canteen/app/Models/Akun.php
@@ -1,13 +1,11 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Akun extends Authenticatable
 {
     protected $table = 'akun'; 
-
     protected $fillable = [
         'nama',
         'email',
@@ -15,14 +13,13 @@ class Akun extends Authenticatable
         'no_telepon',
         'role'
     ];
-
     protected $hidden = [
         'password'
     ];
 
     // biar login pakai "nama"
-    public function getAuthIdentifierName()
-    {
-        return 'nama';
-    }
+    // public function getAuthIdentifierName()
+    // {
+    //     return 'nama';
+    // }
 }
